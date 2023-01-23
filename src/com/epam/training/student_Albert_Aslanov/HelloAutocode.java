61a835879eb91433b989f1962ef7c49135267927
Albert <assasinsalbert@gmail.com>
2023-01-23 15:17:50 +0500

    Java project

---
+++ src/com/epam/training/student_Albert_Aslanov/HelloAutocode.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_Albert_Aslanov;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+    }
+}
